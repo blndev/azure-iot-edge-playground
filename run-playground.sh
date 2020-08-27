a0fc45cdda661bb5d5e58f684c14dd57038a3d4f
blndev <blndev@users.noreply.github.com>
2020-08-27 13:04:19 +0200

    add doc

--- run-playground.sh
+++ run-playground.sh
@@ -1,15 +1,29 @@
+# *********************************************************
+# Initialization of the Playground 
+# ---------------------------------------------------------
+# make sure that you have created Service Principal and 
+# configured the environment variables. 
+# See credentials-template
+# *********************************************************
+
+
 # load environment variables to build the playground
 source credentials-apply.sh
 # create cloud resources
 terraform apply --auto-approve
 
-#create  enrollment group with symetric key
-az iot dps enrollment-group create -g {resource_group_name} --dps-name {dps_name} --enrollment-id {enrollment_id} --primary-key {primary_key} --secondary-key {secondary_key}
-vagrant up iot-edge-1 iot-edge-2
+# ! *********************************************************
+# ! Azure ARM provider for Terraform doesn’t support managing
+# ! DPS enrollment groups. For now, you’ll need to use the 
+# ! Azure CLI (Aug 2020)
+# ! This is done in Terraform as well
+# ! *********************************************************
+
+vagrant up
 
 #create enrollment group with x509 certificate
-az iot dps enrollment-group create -g {resource_group_name} --dps-name {dps_name} --enrollment-id {enrollment_id} --primary-key {primary_key} --secondary-key {secondary_key}
-vagrant up iot-edge-3 iot-edge-4
+#az iot dps enrollment-group create -g {resource_group_name} --dps-name {dps_name} --enrollment-id {enrollment_id} --primary-key {primary_key} --secondary-key {secondary_key}
+#vagrant up iot-edge-3 iot-edge-4
 
 
 
