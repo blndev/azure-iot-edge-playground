3272ddf650e0d98d03cabf9b9b830ca69644b5d4
blndev <blndev@users.noreply.github.com>
2020-09-01 16:21:02 +0200

    add required packages for build of sdk

--- vagrant_init_vtpm2.sh
+++ vagrant_init_vtpm2.sh
@@ -1,10 +1,13 @@
 # https://snapcraft.io/install/tpm2-simulator-chrisccoulson/ubuntu
+sudo apt install -y tpm2-tools
 sudo snap install tpm2-simulator-chrisccoulson --edge
+tpm2-simulator-chrisccoulson.tpm2-simulator &
 tpm2_listpcrs -g 0xB
 # Install Azure IoT Edge SDK for TPM Tools
 # https://docs.microsoft.com/en-us/azure/iot-edge/how-to-auto-provision-simulated-device-linux
 sudo apt install -y software-properties-common
-sudo apt install -y cmake build-essential 
+sudo apt install -y cmake build-essential pkg-config
+sudo apt install -y libcurl4-openssl-dev uuid-dev
 git clone https://github.com/Azure/azure-iot-sdk-c.git
 cd azure-iot-sdk-c
 # latest LTS (check github release tags)
