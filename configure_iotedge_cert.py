dddcaef8845db3ae1dfd8d973fa98cf885958d46
blndev <blndev@users.noreply.github.com>
2020-09-02 15:18:51 +0200

    finalize certificate based provisioning on edge side

--- configure_iotedge_cert.py
+++ configure_iotedge_cert.py
@@ -4,30 +4,27 @@
 
 import yaml
 import socket
-filename=socket.gethostname()
+edge_name=socket.gethostname()
 
 from shutil import copyfile
 copyfile('/etc/iotedge/config.yaml', '/etc/iotedge/config.yaml.org')
+copyfile('./config/certs/*'+edge_name+'*', '/etc/iotedge/')
 
 with open('/etc/iotedge/config.yaml') as f:
     data = yaml.load(f)
 with open('./config/iotedge/scope_id') as fsid:
     sid=fsid.read().replace('\n', '')
-with open('./config/iotedge/'+filename+'.key') as fkey:
-    key=fkey.read().replace('\n', '')
-with open('./config/iotedge/' + filename + '.reg') as freg:
-    reg=freg.read().replace('\n', '')
 
 data['provisioning']['source']='dps'
 data['provisioning']['scope_id']=sid
 data['provisioning']['global_endpoint']='https://global.azure-devices-provisioning.net'
 data['provisioning']['attestation']={}
-data['provisioning']['attestation']['method'] = 'symmetric_key'
-data['provisioning']['attestation']['registration_id'] = reg
-data['provisioning']['attestation']['symmetric_key'] = key
+data['provisioning']['attestation']['method'] = 'x509'
+#data['provisioning']['attestation']['registration_id'] = reg
+data['provisioning']['attestation']['identity_cert'] = 'file:///etc/iotedge/iot-edge-device-identity-'+ edge_name +'-full-chain.cert.pem'
+data['provisioning']['attestation']['identity_pk'] = 'file:///etc/iotedge/iot-edge-device-identity-'+ edge_name +'.key.pem'
 
 with open('/etc/iotedge/config.yaml', 'w') as w:
-    
     yaml.dump(data, w, default_flow_style=False)
 
 print (data)
